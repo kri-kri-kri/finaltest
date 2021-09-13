89adf4151d7d5f94dea14aeac8ad09540a55bde2
Gleb Golotvin <golotvin@ians.aero>
2021-09-13 11:40:00 +0300

    up to lesson 4.2 step 8

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,27 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser): 
-    link = "http://selenium1py.pythonanywhere.com/"    
+    link = "http://selenium1py.pythonanywhere.com"    
     page = MainPage(browser, link)   
-    page.open()                      
-    page.should_be_login_link() 
+    page.open()   
+
+def test_guest_can_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()    
+
